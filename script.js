932e3a749ed5152757205ef646afb2718b654d21
JainVarghese <85052695+JainVarghese@users.noreply.github.com>
2022-11-04 10:16:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var card1 =document.getElementById("card1");
+
+function openRegister(){
+    card1.style.transform ="rotateY(180deg)";
+}
+function openLogin(){
+    card1.style.transform ="rotateY(0deg)";
+}
